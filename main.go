6da3a3c73d51275f565664929959d053db7e952d
FabsMS <fabricio.moreno@academico.ifpb.edu.br>
2025-11-16 22:38:05 -0300

    feat: initialize configs

--- main.go
+++ main.go
@@ -1,8 +1,21 @@
 package main
 
-import "github.com/FabsMS/Gopportunies/router"
+import (
+	"fmt"
+
+	"github.com/FabsMS/Gopportunies/config"
+	"github.com/FabsMS/Gopportunies/router"
+)
 
 func main() {
+
+	// Inicializando Configs
+	err := config.Init()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
 	// Inicializando o Router
 	router.Initializer()
 }
